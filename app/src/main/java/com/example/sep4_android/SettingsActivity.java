e063fa410575ed2c50c24692c663b06afe6557c7
Bladecaller <aleksievborislav@abv.bg>
2021-12-02 16:33:33 +0200

    Spinner

--- app/src/main/java/com/example/sep4_android/SettingsActivity.java
+++ app/src/main/java/com/example/sep4_android/SettingsActivity.java
@@ -3,8 +3,11 @@ package com.example.sep4_android;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +18,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.room.entity.Account;
@@ -29,6 +33,7 @@ import viewmodel.TemperatureViewModel;
 
 public class SettingsActivity extends AppCompatActivity {
     EditText location;
+    Spinner spinner;
     Button buttonSet;
     Button buttonBack;
     private HumidityViewModel humidityViewModel;
@@ -41,13 +46,22 @@ public class SettingsActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-        location = findViewById(R.id.locationInput);
         buttonSet = findViewById(R.id.location_button);
         buttonBack = findViewById(R.id.backSettings);
         accountRepoViewModel = new ViewModelProvider(this).get(AccountRepoViewModel.class);
         humidityViewModel = new ViewModelProvider(this).get(HumidityViewModel.class);
         temperatureViewModel = new ViewModelProvider(this).get(TemperatureViewModel.class);
         precipitationViewModel = new ViewModelProvider(this).get(PrecipitationViewModel.class);
+        spinner = findViewById(R.id.spinner);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Horsens");
+        arrayList.add("Aarhus");
+        arrayList.add("Attu");
+        arrayList.add("Nuuk");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,                         android.R.layout.simple_spinner_item, arrayList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
 
         buttonSet.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -56,7 +70,7 @@ public class SettingsActivity extends AppCompatActivity {
                     accountRepoViewModel.addAccount(new Account(1,"user1","userpass",
                             "userMail","sch","Aarhus","West"));
                 }
-                accs.get(accs.size()-1).setLocation(String.valueOf(location.getText()));
+                accs.get(accs.size()-1).setLocation(spinner.getSelectedItem().toString());
                 accountRepoViewModel.addAccount(accs.get(accs.size()-1));
             }
         });
